e6a65d5604f5b5eebe54c8face9d739217918f61
SAURABH KUMAR <142216593+SaurabhSingh218@users.noreply.github.com>
2025-04-26 23:01:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 import pickle
 import string
-from nltk.corpus import stopwords
 import nltk
+from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 
-ps = PorterStemmer()
+# Download punkt and stopwords only once, at startup
+nltk.download('punkt')
+nltk.download('stopwords')
 
+ps = PorterStemmer()
 
 def transform_text(text):
     text = text.lower()
@@ -32,24 +35,25 @@ def transform_text(text):
 
     return " ".join(y)
 
-tfidf = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('model.pkl','rb'))
+# Load models once
+tfidf = pickle.load(open('vectorizer.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb'))
 
 st.title("Email/SMS Spam Classifier")
 
 input_sms = st.text_area("Enter the message")
-nltk.download('punkt')
-nltk.download('stopwords')
+
 if st.button('Predict'):
 
-    # 1. preprocess
+    # 1. Preprocess
     transformed_sms = transform_text(input_sms)
-    # 2. vectorize
+    # 2. Vectorize
     vector_input = tfidf.transform([transformed_sms])
-    # 3. predict
+    # 3. Predict
     result = model.predict(vector_input)[0]
     # 4. Display
     if result == 1:
         st.header("Spam")
     else:
         st.header("Not Spam")
+
